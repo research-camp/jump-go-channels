c1777dc3ddab748e064dbfbd90465615c908108c
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-12 12:13:41 +0330

    add: buffer type

--- internal/channel.go
+++ internal/channel.go
@@ -7,3 +7,23 @@ type Channel interface {
 	Close()
 	Next() bool
 }
+
+// Buffer interface, contains information of channel buffer
+type Buffer interface {
+	Enqueue(interface{}) error
+	Dequeue() interface{}
+	IsFull() bool
+	IsEmpty() bool
+}
+
+type options struct {
+	buffer Buffer
+}
+
+func WithBuffer(b Buffer) Option {
+	return func(o *options) {
+		o.buffer = b
+	}
+}
+
+type Option func(o *options)
