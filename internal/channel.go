78d42aad4d60a4c8514f7bd84e72e1e751fd2bf8
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-15 08:49:44 +0330

    remove: extra inputs from channel creation

--- internal/channel.go
+++ internal/channel.go
@@ -2,51 +2,36 @@ package internal
 
 import "container/list"
 
-// Channel interface, we show channel operations as an interface
-type Channel interface {
-	Send(interface{})
-	Recv() (interface{}, bool)
-	Close()
-	Next() bool
-}
-
-// Buffer interface, contains information of channel buffer
-type Buffer interface {
-	Enqueue(interface{}) error
-	Dequeue() interface{}
-	IsFull() bool
-	IsEmpty() bool
-}
-
-type options struct {
-	buffer Buffer
-}
-
-func WithBuffer(b Buffer) Option {
-	return func(o *options) {
-		o.buffer = b
-	}
-}
-
-type Option func(o *options)
-
-func NewChannel(size int, opts ...Option) Channel {
-	o := &options{
-		buffer: newListBuffer(size),
+type (
+	// Channel interface, we show channel operations as an interface.
+	Channel interface {
+		Send(interface{})
+		Recv() (interface{}, bool)
+		Close()
+		Next() bool
 	}
 
-	for _, f := range opts {
-		f(o)
+	// Buffer interface, contains information of channel buffer.
+	Buffer interface {
+		Enqueue(interface{}) error
+		Dequeue() interface{}
+		IsFull() bool
+		IsEmpty() bool
 	}
+)
 
+// NewChannel builds a new channel for us
+func NewChannel(size int) Channel {
+	// create buffered channel
 	if size > 0 {
 		return &BufferedChannel{
-			buf:   o.buffer,
+			buf:   newListBuffer(size),
 			sendQ: new(list.List).Init(),
 			recvQ: new(list.List).Init(),
 		}
 	}
 
+	// create unbuffered channel
 	return &SyncChan{
 		sendQ: new(list.List).Init(),
 		recvQ: new(list.List).Init(),
