9976cde3cb7d849d475446fecfa0e8064c518624
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-16 09:15:40 +0330

    add: schedule method of scheduler

--- internal/scheduler.go
+++ internal/scheduler.go
@@ -1,4 +1,20 @@
 package internal
 
-type scheduler struct {
+import "container/list"
+
+type scheduler struct{}
+
+func (s scheduler) Schedule(queue *list.List) *list.List {
+	// creating a new list
+	newQueue := new(list.List).Init()
+
+	// get queue size
+	size := queue.Len()
+
+	// scheduling logic in here
+	for i := 0; i < size; i++ {
+		newQueue.PushBack(queue.Back())
+	}
+
+	return newQueue
 }
