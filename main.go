e014c138a5da85a6f183eeb33f35e5dc1332c0e7
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-16 09:37:03 +0330

    bug: in scheduling

--- main.go
+++ main.go
@@ -7,22 +7,42 @@ import (
 	"github.com/research-camp/go-channels-scheduling/internal"
 )
 
+type Data struct {
+	Value string
+	p     int
+}
+
+func (d Data) Priority() int {
+	return d.p
+}
+
 func main() {
 	// unbuffered channel
-	ch := internal.NewChannel(0)
+	ch := internal.NewChannel(2, true)
 	wg := sync.WaitGroup{}
 
-	wg.Add(1)
+	wg.Add(2)
+
+	ch.Send(Data{
+		Value: "low value",
+		p:     1,
+	})
+	ch.Send(Data{
+		Value: "high value",
+		p:     2,
+	})
 
 	// create a go routine
 	go func() {
-		msg, _ := ch.Recv()
+		for ch.Next() {
+			msg, _ := ch.Recv()
 
-		log.Println(msg)
-		wg.Done()
+			log.Println(msg.(Data).Value)
+
+			wg.Done()
+		}
 	}()
 
-	ch.Send("Hello World")
-
 	wg.Wait()
+	ch.Close()
 }
