6a375426dd9b34847478df237bb47f25c6f2f29f
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-17 10:22:20 +0330

    add: schedule in sync init

--- internal/channel.go
+++ internal/channel.go
@@ -33,7 +33,8 @@ func NewChannel(size int, schedule bool) Channel {
 
 	// create unbuffered channel
 	return &SyncChan{
-		sendQ: new(list.List).Init(),
-		recvQ: new(list.List).Init(),
+		schedule: schedule,
+		sendQ:    new(list.List).Init(),
+		recvQ:    new(list.List).Init(),
 	}
 }
