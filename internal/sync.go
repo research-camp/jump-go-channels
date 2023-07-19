267509d41a0afecd892f07a8830c2ca0cfbbaf5b
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-19 11:00:11 +0330

    update: sync schedule

--- internal/sync.go
+++ internal/sync.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"container/list"
-	"github.com/research-camp/go-channels-scheduling/pkg"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -72,32 +71,15 @@ func (c *SyncChan) Send(val interface{}) {
 
 	ticket := atomic.AddInt32(&c.sendCounter, 1)
 
-	if c.schedule {
-		t := &token{
-			value: ticket,
-			p:     val.(pkg.Schedulable).Priority(),
-		}
-
-		c.sendQ.PushBack(t)
-
-		c.sendQ = schedule(c.sendQ)
-	} else {
-		c.sendQ.PushBack(ticket)
-	}
+	c.sendQ.PushBack(ticket)
 
 	c.lock.Unlock()
 
 	for {
 		c.lock.Lock()
 
-		if c.schedule {
-			if c.val == nil && ticket == c.sendQ.Front().Value.(*token).value {
-				break
-			}
-		} else {
-			if c.val == nil && ticket == c.sendQ.Front().Value {
-				break
-			}
+		if c.val == nil && ticket == c.sendQ.Front().Value {
+			break
 		}
 
 		c.lock.Unlock()
