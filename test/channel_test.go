7ed33096275bde609e3fa32c3c7595301a983b9c
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-09 09:30:10 +0330

    add: channel testing

--- test/channel_test.go
+++ test/channel_test.go
@@ -1,1 +1,37 @@
 package test
+
+import (
+	"sync"
+	"testing"
+)
+
+type dataMock struct {
+	Value int
+}
+
+func (d dataMock) Priority() int {
+	return d.Value
+}
+
+func BenchmarkChannel(b *testing.B) {
+	wg := sync.WaitGroup{}
+	ch := make(chan dataMock)
+
+	go func() {
+		for {
+			<-ch
+
+			wg.Done()
+		}
+	}()
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+
+		ch <- dataMock{
+			Value: i,
+		}
+	}
+
+	wg.Wait()
+}
