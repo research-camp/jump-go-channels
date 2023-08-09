1224b182f0bd4da38a482ded4580a7e57ea80dbb
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-09 13:14:58 +0330

    fix: bugs of tests

--- test/channel_test.go
+++ test/channel_test.go
@@ -1,5 +1,10 @@
 package test
 
+import (
+	"sync"
+	"testing"
+)
+
 type dataMock struct {
 	Value int
 }
@@ -8,25 +13,25 @@ func (d dataMock) Priority() int {
 	return d.Value
 }
 
-//func BenchmarkChannel(b *testing.B) {
-//	wg := sync.WaitGroup{}
-//	ch := make(chan dataMock)
-//
-//	go func() {
-//		for {
-//			<-ch
-//
-//			wg.Done()
-//		}
-//	}()
-//
-//	for i := 0; i < b.N; i++ {
-//		wg.Add(1)
-//
-//		ch <- dataMock{
-//			Value: b.N - i,
-//		}
-//	}
-//
-//	wg.Wait()
-//}
+func BenchmarkChannel(b *testing.B) {
+	wg := sync.WaitGroup{}
+	ch := make(chan dataMock)
+
+	go func() {
+		for {
+			<-ch
+
+			wg.Done()
+		}
+	}()
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+
+		ch <- dataMock{
+			Value: b.N - i,
+		}
+	}
+
+	wg.Wait()
+}
