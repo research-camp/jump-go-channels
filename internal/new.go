dd7fa9cea0e1860b66d638ea2467b2ad47bef0fb
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-22 12:43:00 +0330

    add: on new handlers

--- internal/new.go
+++ internal/new.go
@@ -1,25 +1,38 @@
 package internal
 
+import (
+	"sync"
+	"time"
+)
+
 // Wrapper works as a scheduler on our channel
 type Wrapper struct {
 	InChannel  chan interface{}
 	OutChannel chan interface{}
 
+	lock     sync.Mutex
+	count    int
+	limit    int
+	schedule bool
+
 	channel chan interface{}
 }
 
 // New returns a new channel
-func New(size int) *Wrapper {
+func New(schedule bool, size int) *Wrapper {
 	w := Wrapper{
-		channel: make(chan interface{}),
+		channel:  make(chan interface{}),
+		schedule: schedule,
 	}
 
 	if size > 0 {
 		w.InChannel = make(chan interface{}, size)
 		w.OutChannel = make(chan interface{}, size)
+		w.limit = size
 	} else {
 		w.InChannel = make(chan interface{})
 		w.OutChannel = make(chan interface{})
+		w.limit = 1
 	}
 
 	go w.onSend()
@@ -29,15 +42,39 @@ func New(size int) *Wrapper {
 }
 
 // onSend handler
-func (w Wrapper) onSend() {
+func (w *Wrapper) onSend() {
 	for {
-		w.channel <- w.InChannel
+		val := <-w.InChannel
+
+		for {
+			w.lock.Lock()
+
+			if w.count < w.limit {
+				w.count++
+
+				break
+			}
+
+			w.lock.Unlock()
+
+			time.Sleep(1 * time.Microsecond)
+		}
+
+		w.channel <- val
 	}
 }
 
 // onRecv handler
-func (w Wrapper) onRecv() {
+func (w *Wrapper) onRecv() {
 	for {
-		w.OutChannel <- w.channel
+		val := <-w.channel
+
+		w.lock.Lock()
+
+		w.count--
+
+		w.lock.Unlock()
+
+		w.OutChannel <- val
 	}
 }
